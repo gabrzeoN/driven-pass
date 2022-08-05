ae88188dcce27671ee4e98c2ae18dbedf061a2ed
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-05 11:01:13 -0300

    Preparando revisão de código

--- src/schemas/annotationsSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi";
-import { InputData } from "../repositories/annotationsRepository.js";
-
-export const createSchema = joi.object<InputData>({
-    title: joi.string().trim().max(50).required(),
-    note: joi.string().max(1000).trim().required()
-});
